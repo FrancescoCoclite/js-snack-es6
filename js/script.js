a01b405b1cda3aab4a855d17eb9b6c1ceea7dd38
Francesco <francescococlite@gmail.com>
2021-06-16 15:38:46 +0200

    errore

--- js/script.js
+++ js/script.js
@@ -1,45 +1,45 @@
 // Snack 1 Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
 
-var gruppoBici = [
-    {
-     nome: 'graziella',
-     peso : 6.5
-    },
+// var gruppoBici = [
+//     {
+//      nome: 'graziella',
+//      peso : 6.5
+//     },
 
-    {
-     nome: 'pippo',
-    peso : 3
-    },
+//     {
+//      nome: 'pippo',
+//     peso : 3
+//     },
     
-    {
-     nome: 'bmx',
-     peso : 1
-    },
+//     {
+//      nome: 'bmx',
+//      peso : 1
+//     },
     
-];
+// ];
 
-let pesoMinimo = gruppoBici[0];
+// let pesoMinimo = gruppoBici[0];
 
-for (let i = 1; i< gruppoBici.length; i++){
+// for (let i = 1; i< gruppoBici.length; i++){
     
-    if (gruppoBici[i].peso < pesoMinimo.peso){
-        pesoMinimo = gruppoBici[i];
-    }
-}
+//     if (gruppoBici[i].peso < pesoMinimo.peso){
+//         pesoMinimo = gruppoBici[i];
+//     }
+// }
 
-const {nome,peso} = pesoMinimo
-console.log(peso);
+// const {nome,peso} = pesoMinimo
+// console.log(peso);
 
-document.getElementById('stampa').innerHTML =
+// document.getElementById('stampa').innerHTML =
 
-`
-<ul>
-<li> nome:  ${nome}</li>
+// `
+// <ul>
+// <li> nome:  ${nome}</li>
 
-<li> peso: ${peso}</li>
+// <li> peso: ${peso}</li>
 
-</ul>
-`
+// </ul>
+// `
 // Snack2 Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0. Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti. (utilizzate le arrow function). Infine usando la destrutturazione creiamo un nuovo array i cui elementi sono sempre degli oggetti che contengono solo nomi e falli subiti e stampiamo tutto in console
 
 
@@ -64,15 +64,20 @@ var squadre = [
     },
 ];
 
-
-for(var i =0; i<squadre.length; i++){
-    const puntiRandom = () => squadre[i].punti = Math.floor(Math.random() * 10);
-    const falliRandom = () => squadre[i].falli = Math.floor(Math.random() * 10);
-    puntiRandom();
-    falliRandom();
+const nomePunti = [];
+console.log(nomePunti);
+for(let i =0; i<squadre.length; i++){
+    
+    
+    const random = () => Math.floor(Math.random() * 11);
+    
 };
+random(punti);
+random(falli);
 
 console.log(squadre);
+
+
 
 
 
